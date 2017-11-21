4ba775fcd4a1a46b55bc5e0bbdb760c418761665
Ivaylo Nenovski <ridiculous1995@gmail.com>
2017-11-21 13:32:07 -0500

    Started working on Longana. Implemented a few rules regarding tiles in the game

--- Longana.pl
+++ Longana.pl
@@ -5,4 +5,40 @@
 %* Date:               12/08/2017                           *
 %************************************************************
 
-%Knowledge Base
+%KNOWLEDGE BASE
+
+%RULES
+
+%Tile
+validTile(Pip1, Pip2) :-
+    Pip1 =< 6,
+    Pip1 >= 0,
+    Pip2 =< 6,
+    Pip2 >= 0.
+
+isDoubleTile(Pip1, Pip2) :-
+    Pip1 = Pip2.
+
+%Stock
+
+%Generate stock base case
+generateStock(Pip1, Pip2, ListOutput) :-
+    Pip1 = 6,
+    Pip2 = 6,
+    append([Pip1, Pip2], ListOutput, ListOutput).
+
+%Generate stock recursive case
+generateStock(Pip1, Pip2, ListOutput) :-
+    NewTile = [Pip1, Pip2],
+    append(NewTile, ListOutput, ListOutput),
+    Pip2 = 6,
+    NewPip1 is Pip1 + 1,
+    NewPip2 = 0,
+    generateStock(NewPip1, NewPip2, ListOutput).
+
+generateStock(Pip1, Pip2, ListOutput) :-
+    NewTile = [Pip1, Pip2],
+    append(NewTile, ListOutput, ListOutput),
+    NewPip2 is Pip2 + 1,
+    generateStock(Pip1, NewPip2, ListOutput).
+
